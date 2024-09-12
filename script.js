732f1080101eb3ae98e99da101a385b09be2b47a
chansol4517 <ckssol4517@gmail.com>
2024-09-12 13:43:28 +0900

    >> 반복문과 조건문을 활용한 코드 축약_최종

--- script.js
+++ script.js
@@ -14,15 +14,12 @@ console.log("test");
     위의 모든 조건조건이 참이 아닐때 이곳의 구문을 무조건 실행하고 종료
   }
 */
-
 const hour = new Date().getHours();
 console.log(hour);
-
 //0~9 :lightblue
 //10~15 : orange
 //16~20 : pink
 //21~24 : purple
-
 /*
 if (hour >= 0 && hour < 10) {
   document.body.style.backgroundColor = "lightblue";
@@ -34,7 +31,6 @@ if (hour >= 0 && hour < 10) {
   document.body.style.backgroundColor = "purple";
 }
 */
-
 // if (hour >= 0 && hour < 10) document.body.style.backgroundColor = "lightblue";
 // else if (hour >= 10 && hour < 16) document.body.style.backgroundColor = "orange";
 // else if (hour >= 16 && hour < 21) document.body.style.backgroundColor = "pink";
@@ -44,3 +40,20 @@ if (hour >= 0 && hour < 10) document.body.style.backgroundColor = "lightblue";
 if (hour >= 10 && hour < 16) document.body.style.backgroundColor = "orange";
 if (hour >= 16 && hour < 21) document.body.style.backgroundColor = "pink";
 if (hour >= 21 && hour < 25) document.body.style.backgroundColor = "purple";
+const dataArr = [
+	{ cond: hour >= 0 && hour < 10, color: "lightblue" },
+	{ cond: hour >= 10 && hour < 16, color: "violet" },
+	{ cond: hour >= 16 && hour < 21, color: "pink" },
+	{ cond: hour >= 21 && hour < 25, color: "purple" }
+];
+
+dataArr.forEach(data => {
+	if (data.cond) document.body.style.backgroundColor = data.color;
+});
+
+//dataArr.forEach(data => data.cond && (document.body.style.backgroundColor = data.color));
+
+// if (hour >= 0 && hour < 10) document.body.style.backgroundColor = "lightblue";
+// if (hour >= 10 && hour < 16) document.body.style.backgroundColor = "orange";
+// if (hour >= 16 && hour < 21) document.body.style.backgroundColor = "pink";
+// if (hour >= 21 && hour < 25) document.body.style.backgroundColor = "purple";
